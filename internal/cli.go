05630a78c1835eb7466521b65e837460d03bf92f
zadam <zerenadam7@gmail.com>
2026-01-10 18:48:59 -0500

    updated cli.go

--- internal/cli.go
+++ internal/cli.go
@@ -1,22 +1,31 @@
 package internal
 
 import (
-	"flag"
 	"fmt"
 	"os"
 )
 
-func ParseCli() (projectname, gitlink string) {
-	gitLink := flag.String("git", "", "Git remote URL (optional)")
-	flag.Parse()
-
-	args := flag.Args()
+func ParseCli() (projectname, link string) {
+	args := os.Args[1:]
+	gitlink := ""
 
 	if len(args) < 1 {
 		fmt.Println("Usage: goskeleton <projectname> [-git <gitlink>]")
 		os.Exit(1)
 	}
 
-	return args[0], *gitLink
+	for i := 1; i < len(args); i++ {
+		if args[i] == "-git" && i+1 < len(args) {
+			gitlink = args[i+1]
+			i++
+		}
+	}
+
+	fmt.Println("Project Name:", args[0])
+	if gitlink != "" {
+		fmt.Println("Git Link:", gitlink)
+	}
+
+	return args[0], gitlink
 
 }
