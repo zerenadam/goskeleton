abed72a4c1e250ae0e7d16f667f27fd20cf85c80
zadam <zerenadam7@gmail.com>
2026-01-10 19:02:55 -0500

    updated: main.go

--- cmd/goskeleton/main.go
+++ cmd/goskeleton/main.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"github.com/zerenadam/goskeleton/internal"
+)
+
 func main() {
+	projectname, gitlink, gitflag := internal.ParseCli()
+
+	internal.CreateFolders(projectname)
+	internal.CreateFiles(projectname)
+	if gitflag == true {
+		internal.RunGitCommands(projectname, gitlink)
+	}
+
 }
