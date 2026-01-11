da8e921db809c0fed3cf2ec04fe1a00725d511ec
zadam <zerenadam7@gmail.com>
2026-01-10 21:29:13 -0500

    added readme.md

--- internal/files.go
+++ internal/files.go
@@ -19,11 +19,17 @@ func main(){
 	}`
 		gitignore = `.env
 .DS_Store`
+
+		readme = `
+---
+
+[![made with goskeleton](https://img.shields.io/badge/made%20with-goskeleton%20🦴-blue)](https://github.com/zerenadam/goskeleton)`
 	)
 
 	files := map[string]string{
 		filepath.Join(projectname, "cmd", projectname, "main.go"): maingo,
 		filepath.Join(projectname, ".gitignore"):                  gitignore,
+		filepath.Join(projectname, "README.md"):                   readme,
 	}
 
 	for path, content := range files {
